8bbe2ce07cd279a253bd76e834ed3460d0df4def
alstjr5949 <alstjr5949@gmail.com>
2022-10-21 23:45:08 +0900

    feat : todo app setting

--- App.js
+++ App.js
@@ -1,98 +1,28 @@
-import { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 import {
-  ActivityIndicator,
-  Dimensions,
-  ScrollView,
   StyleSheet,
   Text,
   View,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Pressable,
 } from "react-native";
 
-import * as Location from "expo-location";
-
-const API_KEY = "ee5f10276798b0ea2df4f838cea4860d";
-
-const icons = {
-  Clear: "day-sunny",
-  Clouds: "cloudy",
-  Rain: "rain",
-  Atmosphere: "cloudy-gusts",
-  Snow: "snow",
-  Drizzle: "day-rain",
-  Thunderstorm: "lightning",
-};
-
 export default function App() {
-  const [city, setCity] = useState("Loading...");
-  const [daysArr, setDaysArr] = useState([]);
-  const [ok, setOk] = useState(true);
-  const getWeather = async () => {
-    const { granted } = await Location.requestForegroundPermissionsAsync();
-    if (!granted) {
-      setOk(false);
-    }
-
-    const {
-      coords: { latitude, longitude },
-    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
-
-    const location = await Location.reverseGeocodeAsync(
-      { latitude, longitude },
-      { useGoogleMaps: false }
-    );
-    setCity(location[0].region);
-    const res = await fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
-    );
-    const json = await res.json();
-    setDaysArr(json.daily);
-  };
-
-  useEffect(() => {
-    getWeather();
-  }, []);
-
   return (
     <View style={styles.container}>
-      <View style={styles.city}>
-        <Text style={styles.cityName}>{city}</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableOpacity>
       </View>
-      <ScrollView
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        pagingEnabled
-        contentContainerStyle={styles.weather}
-      >
-        {daysArr.length === 0 ? (
-          <View style={styles.day}>
-            <ActivityIndicator size="large" />
-          </View>
-        ) : (
-          days.map((day, index) => (
-            <View key={index} style={styles.day}>
-              <View
-                style={{
-                  width: "100%",
-                  flexDirection: "row",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}
-              >
-                <Text style={styles.temp}>
-                  {parseFloat(day.temp.day).toFixed(1)}
-                </Text>
-                <Fontisto
-                  name={icons[day.weather[0].main]}
-                  size={68}
-                  color="white"
-                />
-              </View>
-              <Text style={styles.description}>{day.weather[0].main}</Text>
-              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
-            </View>
-          ))
-        )}
-      </ScrollView>
     </View>
   );
 }
@@ -100,32 +30,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#eee",
-  },
-  city: {
-    flex: 1.2,
+    backgroundColor: "#fff",
+    alignItems: "center",
     justifyContent: "center",
-    alignItems: "center",
   },
-  cityName: {
-    fontSize: 68,
-    fontWeight: "800",
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
   },
-  weather: {},
-  day: {
-    width: Dimensions.get("window").width,
-    alignItems: "center",
-  },
-  temp: {
-    marginTop: 50,
-    fontSize: 178,
-    fontWeight: "800",
-  },
-  description: {
-    marginTop: -30,
-    fontSize: 50,
-  },
-  tinyText: {
-    fontSize: 20,
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
+    color: "white",
   },
 });
